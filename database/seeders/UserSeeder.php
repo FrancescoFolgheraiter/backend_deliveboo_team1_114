fcf7e657b39d5e4b7f7e3665b06e120aea513228
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-26 13:26:52 +0100

    edit UserSeeder need to compleate

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,11 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Models
+use App\Models\User;
+
+// Helpers 
+use Illuminate\Support\Facades\Schema;
 class UserSeeder extends Seeder
 {
     /**
@@ -12,6 +17,57 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        //primo modo per eseguire in modo sicuro il truncate
+        Schema::disableForeignKeyConstraints();
+        user::truncate();
+        Schema::enableForeignKeyConstraints();
 
+        //nomi plausibili per le attività
+        $name=[
+            'Galloway',
+            'BurgerKong',
+            'MucDonalds',
+            'Da Albert',
+            'Korallo',
+            'Borgo Nuovo',
+            'RoadMouse',
+            'StefanoPizza',
+            'Cinatown',
+            'Da Mario',
+            'Fork',
+            'Mindujo',
+            'Pizza Paradise',
+            'Burger Bistro',
+            'Pizzeria Deliziosa',
+            'Slice & Grill',
+            'Crispy Crust Cafe',
+            'Pizzamania',
+            'Burger Bonanza',
+            'Burger Barbecue',
+            'Sushi Sinfonia',
+            'La Bella Italia',
+            'Sushi Palace'
+        ];
+
+        //popolazione table  user
+        for ($i=0; $i < 20; $i++) {
+            //prelevo un nome random dall'array nomi e poi lo elimino
+            $randomIndex=random_int(0,count($name));
+            $resturant_name = $name[$randomIndex];
+            unset($name[$randomIndex]);
+            //fine gestione nomi
+
+            $email = fake()->email;
+            $password = 'password';
+            $address = fake() -> address;
+
+            //inizializzo a stringa vuota vatNumber
+            $vatNumber = '';
+            for ($i=0; $i < 11; $i++) { 
+                $vatNumber .= rand(0,9);
+            };
+
+
+        }
     }
 }

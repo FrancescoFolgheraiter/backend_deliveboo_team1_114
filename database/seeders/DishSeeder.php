7b0ea66f6fbcadf7f5fb90c36c22da74ee1b8e64
MironAlberto <alberto.miron19@gmail.com>
2024-03-26 12:35:16 +0100

    started working on dish seeder

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Models
+use App\Models\Dish;
+
 class DishSeeder extends Seeder
 {
     /**
@@ -12,6 +15,18 @@ class DishSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Dish::truncate();
+
+        for ($i = 0; $i < 30, $i++){
+            $dish = new Dish();
+
+            $dish->name = 
+            $dish->description = 
+            $dish->ingredients
+            $dish->price 
+            $dish->visible
+            $dish->image 
+            $dish->save();
+        }
     }
 }

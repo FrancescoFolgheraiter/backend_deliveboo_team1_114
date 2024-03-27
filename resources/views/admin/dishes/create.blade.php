5ff23a530e28ee49843dcdd75a356530c7e20dc1
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-27 21:04:03 +0100

    create view create dish and update with view function create DishController

---
+++ resources/views/admin/dishes/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('page-title', 'Tutti i piatti')
+
+@section('main-content')
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                create
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

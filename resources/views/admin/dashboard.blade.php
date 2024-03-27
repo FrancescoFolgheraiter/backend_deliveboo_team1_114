be8e875aaf206895ca1055e6f7e5e509f6907f76
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-27 16:16:16 +0100

    create structure dashboard user

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,13 +5,40 @@
 @section('main-content')
     <div class="row">
         <div class="col">
-            <div class="card">
+            <div class="card my-user-card">
                 <div class="card-body">
                     <h1 class="text-center text-success">
-                        Sei loggato!
+                        Nome del locale
                     </h1>
-                    <br>
-                    La dashboard è una pagina privata (protetta dal middleware)
+                    <hr>
+                    <!--contenuto principale di interazione utente-->
+                    <div class="row h-75">
+                        <div class="col-md-3 col-12 h-100 ">
+                            <aside class="d-flex flex-column justify-content-around h-100">
+                                <div>
+                                    Località
+                                </div>
+                                <div>
+                                    Tipologià di ristorante
+                                </div>
+                                <div>
+                                    Immagine di copertina
+                                </div>
+                            </aside>
+                        </div>
+                        <div class="col-md-9 col-12 d-flex flex-column justify-content-between ">
+                            <div>
+                                Vai alla modifica dei tuoi piatit
+                            </div>
+                            <div>
+                                Ordini di oggi 
+                            </div>
+                            <div>
+                                Vedi storico ordini
+                            </div>      
+                        </div>
+                    </div>
+                    <!--fine contenuto principale di interazione utente-->
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,13 +21,13 @@
                     <div class="collapse navbar-collapse" id="navbarText">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
+                                <a class="nav-link" href="{{ route('admin.dashboard') }}">Pagina del ristorante</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link 2</a>
+                                <a class="nav-link" href="#">Lista piatti</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Link 3</a>
+                                <a class="nav-link" href="#">Ordini</a>
                             </li>
                         </ul>
                         <form method="POST" action="{{ route('logout') }}">

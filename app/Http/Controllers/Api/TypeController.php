326d52bdd0ba4fd9ab16fabb5c967b6436717fe7
MironAlberto <alberto.miron19@gmail.com>
2024-03-28 16:56:24 +0100

    added api routes, type-controller and changed allowed origins in cors.php (remember to update ENV)

---
+++ app/Http/Controllers/Api/TypeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Models 
+use App\Models\Type;
+
+class TypeController extends Controller
+{
+    public function index()
+    {
+        $types = Type::with(['users' => function ($query) {
+            $query->select('id', 'resturant_name', 'address', 'email');
+        }]);
+
+        return response()->json([
+            'code' => 200,
+            'message' => 'ok',
+            'data' => [
+                'types' => $types
+            ]
+        ]);
+    }
+}

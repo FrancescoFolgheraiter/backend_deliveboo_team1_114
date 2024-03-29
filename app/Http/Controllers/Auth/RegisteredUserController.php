ebffe517d6d8a3dd00ab04a2f59e98255ce2439a
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-29 09:58:44 +0100

    update registerUserController

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,16 +36,18 @@ class RegisteredUserController extends Controller
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'address'=>['required', 'string', 'max:255'],
             'vat_number'=>['required', 'string', 'max:255'],
-            'resturant_image'=>['required', 'string', 'max:255'],
+            'resturant_image'=>['required', 'image', 'max:2048'],
         ]);
+        
+        $imagePath = $request->file('resturant_image')->store('img/user', 'public');
 
         $user = User::create([
-            'name' => $request->name,
+            'resturant_name' => $request->resturant_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
             'address' => $request->address,
             'vat_number' => $request->vat_number,
-            'resturant_image' => $request->resturant_image,
+            'resturant_image'=>$imagePath 
         ]);
 
         event(new Registered($user));

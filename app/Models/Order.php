886635c3dc268b4b15173b36939a3b5a90920c55
MironAlberto <alberto.miron19@gmail.com>
2024-03-29 15:52:43 +0100

    added pivot 'quantity' to relationship -> need to fix accessor

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,11 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-
+    
     // Relationships
 
     public function dishes() {
 
-        return $this->belongsToMany(Dish::class);
+        return $this->belongsToMany(Dish::class)->withPivot('quantity');
     }
+
+    // // grazie ad accessor e usango get...Attribute 
+    // // riesco a calcolarmi in modo dinamico il prezzo totale
+    // public function getTotalPriceAttribute(): float
+    // {   
+    //     // inizializzo a 0
+    //     $totalPrice = 0;
+
+    //     // itero su dishes 
+    //     foreach ($this->dishes as $dish) {
+        
+    //         // moltiplico i piatti per il numero degli ordini
+    //         $totalPrice += $dish->price * $dish->pivot->quantity;
+    //     }
+
+    //     // return $totalPrice;
+    //     dd($totalPrice);
+
+    //     return $totalPrice;
+    // }
 }

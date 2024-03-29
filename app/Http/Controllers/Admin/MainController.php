9ca05a32cdf669bafa853831306fb5997113980f
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-03-29 12:30:49 +0100

    update MainController with new function for view editTypes of user

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -9,13 +9,21 @@ use Illuminate\Http\Request;
 use App\Models\type;
 class MainController extends Controller
 {
-
+    //funzione che permette di visualizzare i dati dell'utente le categhorie ad esso collegate
     public function dashboard()
     {
         $user = auth()->user();
         $types = Type::all();
         return view('admin.dashboard', compact('user','types'));
     }
+    //
+    public function types(){
+        $user = auth()->user();
+        $types = Type::all();
+        return view('admin.editTypes', compact('user','types'));
+    }
+   
+
 
 
 }

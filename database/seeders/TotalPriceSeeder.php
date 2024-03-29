32efa039471182630e79479dc1b5cd4457bb3d81
MironAlberto <alberto.miron19@gmail.com>
2024-03-29 17:27:00 +0100

    added fake date

--- database/seeders/TotalPriceSeeder.php
+++ database/seeders/TotalPriceSeeder.php
@@ -18,7 +18,8 @@ class TotalPriceSeeder extends Seeder
         $orders = Order::all();
         
         foreach ($orders as $order){
-            $order->total_price = $order->dishes;
+            $order->date = fake()->dateTimeBetween('2024-01-01', 'now')->format('Y-m-d H:i:s');
+            // $order->total_price = $order->dishes;
             // dd($order->dishes);
             $totalPrice = 0;
             foreach ($order->dishes as $dish){

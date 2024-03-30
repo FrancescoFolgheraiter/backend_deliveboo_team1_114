3620bee81f6c7d752cbd38428ca99f02b5da86a1
MironAlberto <alberto.miron19@gmail.com>
2024-03-30 18:25:10 +0100

    modified layout for login and register -> need to review it

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,98 +3,93 @@
 @section('page-title', 'Register')
 
 @section('main-content')
-    <div class="text-center">
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <div class="mb-0">
-                    @foreach ($errors->all() as $error)
-                        <div>
-                            {{ $error }}
+    <div class="custom-container">
+        <div class="p-4 custom-card-register">
+            <div class="text-center text-color">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <div class="mb-0">
+                            @foreach ($errors->all() as $error)
+                                <div>
+                                    {{ $error }}
+                                </div>
+                            @endforeach
                         </div>
-                    @endforeach
-                </div>
-            </div>
-        @endif
-        <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
-            @csrf
+                    </div>
+                @endif
+                <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+                    @csrf
+                    
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <!-- Email Address -->
+                            <div class="form-floating">
+                                <input type="email" name="email" class="form-control" id="email" placeholder="Email">
+                                <label for="email">Email</label>
+                            </div>
     
-            <!-- Name -->
-            <div>
-                <label for="resturant_name" class="fw-bolder">
-                    Nome del locale
-                </label>
-                <div>
-                    <input type="text" id="resturant_name" name="resturant_name">
-                </div>
-            </div>
+                            <!--Adress-->
+                            <div class="mt-4 form-floating">
+                                <input type="text" id="address" name="address" class="form-control">
+                                <label for="address">
+                                    Indirizzo
+                                </label>
+                            </div>
     
-            <!-- Email Address -->
-            <div class="mt-4">
-                <label for="email" class="fw-bolder">
-                    Email
-                </label>
-                <div>
-                    <input type="email" id="email" name="email">
-                </div>
-            </div>
+                            <!-- Password -->
+                            <div class="mt-4 form-floating">
+                                <input type="password" id="password" name="password" class="form-control">
+                                <label for="password">
+                                    Password
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <!-- Name -->
+                            <div class="form-floating">
+                                <input type="text" id="resturant_name" name="resturant_name" class="form-control">
+                                <label for="resturant_name">
+                                    Nome del locale
+                                </label>
+                            </div>
     
-            <!-- Password -->
-            <div class="mt-4">
-                <label for="password" class="fw-bolder">
-                    Password
-                </label>
-                <div>
-                    <input type="password" id="password" name="password">
-                </div>
-            </div>
+                            <!--Partita Iva-->
+                            <div class="mt-4 form-floating">
+                                <input type="number" id="vat_number" name="vat_number" class="form-control">
+                                <label for="vat_number">
+                                    Partita IVA
+                                </label>
+                            </div>
     
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <label for="password_confirmation" class="fw-bolder">
-                    Conferma Password
-                </label>
-                <div>
-                    <input type="password" id="password_confirmation" name="password_confirmation">
-                </div>
-            </div>
-
-            <!--Adress-->
-            <div class="mt-4">
-                <label for="address" class="fw-bolder">
-                    Indirizzo
-                </label>
-                <div>
-                    <input type="text" id="address" name="address">
-                </div>
-            </div>
-
-            <!--Partita Iva-->
-            <div class="mt-4">
-                <label for="vat_number" class="fw-bolder">
-                    Partita IVA
-                </label>
-                <div>
-                    <input type="number" id="vat_number" name="vat_number">
-                </div>
-            </div>
-
-            <!--immagine del locale-->
-            <div class="mt-4">
-                <label for="resturant_image" class="form-label fw-bolder"> Carica l'immagine del locale</label>
-                <input class="form-control" type="file" id="resturant_image" name="resturant_image">
-            </div>
+                            <!-- Confirm Password -->
+                            <div class="mt-4 form-floating">
+                                <input type="password" id="password_confirmation" name="password_confirmation" class="form-control">
+                                <label for="password_confirmation">
+                                    Conferma Password
+                                </label>
+                            </div>
+                        </div>
+                    </div>
     
-            <div class="mt-4">
-                <button type="submit" class="btn btn-outline-light fw-bolder">
-                    Registrati
-                </button>
-
-                <div class="mt-3 pb-3">
-                    <a href="{{ route('login') }}" class="link-light">
-                        {{ __('Already registered?') }}
-                    </a>
-                </div>
+                    <!--immagine del locale-->
+                    <div class="mt-4">
+                        <label for="resturant_image" class="form-label fw-bolder"> Carica l'immagine del locale</label>
+                        <input class="form-control" type="file" id="resturant_image" name="resturant_image">
+                    </div>
+            
+                    <div class="mt-4">
+                        <button type="submit" class="btn button-color fw-bolder text-white">
+                            Registrati
+                        </button>
+        
+                        <div class="mt-3 pb-3">
+                            <a href="{{ route('login') }}" class="link-dark">
+                                {{ __('Già registrato?') }}
+                            </a>
+                        </div>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>
     </div>
 @endsection

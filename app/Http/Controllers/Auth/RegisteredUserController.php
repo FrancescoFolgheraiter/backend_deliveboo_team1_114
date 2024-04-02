238a64e0db9c514d2a00c47d5e1704fb2e73adae
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-04-02 16:01:10 +0200

    edit message ITA validate RegistredUserController

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,9 +39,32 @@ class RegisteredUserController extends Controller
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'address'=>['required', 'string', 'max:255'],
-            'vat_number'=>['required', 'string', 'max:255'],
+            'vat_number'=>['required', 'string', 'max:11'],
             'resturant_image'=>['required', 'image', 'max:2048'],
             'types'=>['required','array','exists:types,id'],
+        ],[
+            'resturant_name.required' => 'Il campo Nome del ristorante è obbligatorio.',//messaggi di validazione
+            'resturant_name.string' => 'Il campo Nome del ristorante deve essere una stringa.',
+            'resturant_name.max' => 'Il campo Nome del ristorante non può superare i 255 caratteri.',
+            'email.required' => 'Il campo Email è obbligatorio.',
+            'email.string' => 'Il campo Email deve essere una stringa.',
+            'email.email' => 'Il campo Email deve essere un indirizzo email valido.',
+            'email.max' => 'Il campo Email non può superare i 255 caratteri.',
+            'email.unique' => 'Questo indirizzo email è già stato utilizzato.',
+            'password.required' => 'Il campo Password è obbligatorio.',
+            'password.confirmed' => 'Il campo Password non corrisponde alla conferma.',
+            'address.required' => 'Il campo Indirizzo è obbligatorio.',
+            'address.string' => 'Il campo Indirizzo deve essere una stringa.',
+            'address.max' => 'Il campo Indirizzo non può superare i 255 caratteri.',
+            'vat_number.required' => 'Il campo Partita IVA è obbligatorio.',
+            'vat_number.string' => 'Il campo Partita IVA deve essere una stringa.',
+            'vat_number.max' => 'Il campo Partita IVA non può superare i 11 caratteri.',
+            'resturant_image.required' => 'Il campo Immagine del ristorante è obbligatorio.',
+            'resturant_image.image' => 'Il file Immagine del ristorante deve essere un\'immagine.',
+            'resturant_image.max' => 'Il file Immagine del ristorante non può superare i 2048 kilobyte (2MB).',
+            'types.required' => 'Il campo Tipi è obbligatorio.',
+            'types.array' => 'Il campo Tipi deve essere un array.',
+            'types.exists' => 'Il tipo selezionato non è valido.',
         ]);
         $imagePath = $request->file('resturant_image')->store('img/user', 'public');
 

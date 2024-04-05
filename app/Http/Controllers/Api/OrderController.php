b3a019be41a630d16f9437d46fb5265879901962
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-04-05 12:32:31 +0200

    Create StoreRequest Order and OrcerController API

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//Models
+use App\Models\Order;
+
+class OrderController extends Controller
+{
+    public function store(Request $request)
+    {
+        //
+    }
+
+}

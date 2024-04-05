6d896a4d33fd269e6259366263fe9373094cfc05
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-04-05 12:51:24 +0200

    edit Api OrderController function store

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -8,11 +8,19 @@ use Illuminate\Http\Request;
 //Models
 use App\Models\Order;
 
+//Form request
+use App\Http\Requests\Order\StoreRequest as StoreOrderRequest;
+
 class OrderController extends Controller
 {
-    public function store(Request $request)
+    public function store(StoreOrderRequest $request)
     {
-        //
+        $orderData = $request->validated();
+
+        $order = Order::create($orderData);
+        //invio al frontend di un messaggio di avvenuto
+        //storaggio dei dati con successo
+        return response()->json(['message' => 'Ordine creato con successo', 'order' => $order], 201);
     }
 
 }

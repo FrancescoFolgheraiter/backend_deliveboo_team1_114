c8800b82999ecb9e7b464b489ee911bc07a07abb
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-04-08 13:55:07 +0200

    fixed dishController Api index for filter users

--- app/Http/Controllers/Api/DishController.php
+++ app/Http/Controllers/Api/DishController.php
@@ -8,22 +8,39 @@ use Illuminate\Http\Request;
 
 //Models
 use App\Models\Dish;
+use App\Models\User;
 
 class DishController extends Controller
 {
     //index piatti api
-    public function index()
+    public function index(request $request)
     {
-        //paginate mi permette di suddivide i dati in pagine, gli argomenti nel with() permettono l'Eager Loading
-        $foods = Dish::with(['user' => function ($query) {
-            $query->select('id', 'resturant_name', 'address', 'vat_number');
-        }])->paginate(12);
+        //Verifico che la richiesta effettuata tramite api abbia
+        //come parametro name
+        if (!$request->has('name')) {
+            return response()->json([
+                'code' => 400,
+                'message' => 'Il parametro "name" è obbligatorio',
+            ], 400);
+        }
+        //prendo l'utente che mi è stato richiesto dal db
+        $restaurantId = User::where('name_restaurant', $request->input('name'))->value('id');
+        //Verifico che la ricerca del ristorante sia andata a buon fine
+        if (!$restaurantId) {
+            return response()->json([
+                'code' => 404,
+                'message' => 'Nessun ristorante trovato con il nome specificato',
+            ], 404);
+        }
+        //prendo i piatti di quell'utente
+        $dishes = Dish::where('user_id', $restaurantId)->get();
+
         //tramite questa funzione ritorno il file json con al struttura delineata nelle []
         return response()->json([
             'code'=> 200,
             'message'=>'ok',
             'data'=> [
-                'foods'=> $foods
+                'foods'=> $dishes
             ]
         ]);
     }

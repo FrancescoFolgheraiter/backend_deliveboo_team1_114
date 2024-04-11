f3e0b5b50ba17f3b330ef222aef88ca465cfb7a7
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-04-11 18:35:16 +0200

    update OrderController APi

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -9,15 +9,33 @@ use Illuminate\Http\Request;
 use App\Models\Order;
 
 //Form request
-use App\Http\Requests\Order\StoreRequest as StoreOrderRequest;
+use App\Http\Requests\Api\StoreOrderRequest;
+
+//Helpers
+use Carbon\Carbon;
 
 class OrderController extends Controller
 {
     public function store(StoreOrderRequest $request)
     {
-        $orderData = $request->validated();
-
-        $order = Order::create($orderData);
+        $validatedData = $request->validated();
+        $currentDate = Carbon::now();
+        
+        //salvo i dati dell'ordine
+        $order = Order::create([
+            'name' =>$validatedData['params']['name'],
+            'surname' =>$validatedData['params']['surname'],
+            'date' => $currentDate,
+            'total_price'=>$validatedData['params']['total_price'],
+            'note'=>$validatedData['params']['total_price'],
+            'address' =>$validatedData['params']['address'],
+            'phone_number' =>$validatedData['params']['phone_number'],
+        ]);
+        //una volta creato l'ordine posso fare l'attach 
+        //nella pivot Orders->dishes
+        foreach ($validatedData['params']['order'] as $item) {
+            $order->dishes()->attach($item[0], ['quantity' => $item[1]]);
+        }
         //invio al frontend di un messaggio di avvenuto
         //storaggio dei dati con successo
         return response()->json(['message' => 'Ordine creato con successo', 'order' => $order], 201);

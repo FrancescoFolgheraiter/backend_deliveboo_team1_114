a130dd0dd0f1c62147feaa3e0245a178d815e9fe
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2024-04-16 11:59:23 +0200

    update migration orders with new col processed

--- database/migrations/2024_03_26_101758_create_orders_table.php
+++ database/migrations/2024_03_26_101758_create_orders_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
             $table->id();
             $table->timestamp('date');
             $table->text('note')->nullable();
-
             // prezzo del piatto (max 9999.99 per ora)
             $table->decimal('total_price', 6, 2)->unsigned();
             $table->string('name', 64);
             $table->string('surname', 64);
             $table->string('address', 128);
             $table->string('phone_number', 13);
+            $table->boolean('processed')->default(0);
             $table->timestamps();
         });
     }
